30f6892d8ad52fc2ff28c6a0331c8e654c8db374
corrschi <54493785+corrschi@users.noreply.github.com>
2025-02-27 14:44:22 +0800

    Update Fig1H.R

--- plots/Fig1H.R
+++ plots/Fig1H.R
@@ -1,24 +1,15 @@
 
 ###-------------1. data loading-------------######
 work.dir="./Source data"
-
 setwd(work.dir)
-load("./mdd.filter1.Seurat.RData")
-subset_cells <- mdd.filter1
-DefaultAssay(subset_cells) <- "RNA"
-subset_cells@assays$RNA@data <- subset_cells@assays$RNA@counts
-subset_cells <- NormalizeData(subset_cells)
-subset_cells <- ScaleData(subset_cells, features=VariableFeatures(subset_cells))
 
-MERGE <- subset_cells
-### recovery
+load("./mdd.filter1.Seurat.RData")
+MERGE <- mdd.filter1
+### check data
 png(paste0("mdd.filter1_all_by_harmony_dimplot-ANNO.chi-CheckData.png"), width = 9, height = 5, res = 400, units = "in")
 DimPlot(MERGE,pt.size = 0.1,label=T,label.size=3,cols = color_used,raster=F) 
 dev.off()
-
-MERGE #30062 features across 71565 samples 
-
-####-------2.细胞比例分布-BarPlots------------####
+####-------2.BarPlots------------####
 library(ggsci)
 library(ggplot2)
 
@@ -30,7 +21,7 @@ Idents(dat) <- "cluster"
 dat$group <- factor(dat$group,levels= c("Control","Suicide"))
 dat$sample_num <- gsub("sample","",dat$sample,ignore.case=F)
 
-##将sample单纯处理
+## sample change to only number
 aa <- dat@meta.data[,c("sample","group")]
 bb <- aa%>%group_by(group)%>%unique%>%as.data.frame
 bb$sample_num <- bb$sample%>%gsub("sample","",.)
@@ -39,7 +30,7 @@ control <- bb%>%filter(group=='Control')%>%.$sample_num
 suicide <- bb%>%filter(group=='Suicide')%>%.[["sample_num"]]
 
 dat$sample_num <- factor(dat$sample_num,
-                         levels =  c(bb%>%filter(group=='Control')%>%.$sample_num%>%as.numeric%>%sort, #转化为数字，并排序
+                         levels =  c(bb%>%filter(group=='Control')%>%.$sample_num%>%as.numeric%>%sort,
                                      bb%>%filter(group=='Suicide')%>%.$sample_num%>%as.numeric%>%sort ))
 
 
@@ -51,7 +42,7 @@ color26 <- c("#D9DD6B","#ECEFA4","#D54C4C","#8D2828","#FDD2BF","#E98580","#DF5E5
 
 
 # pdf("Fig1h.clusters Barplots-sample_and_cluster-Anno.chi1.pdf",width = 12,height =6)
-png("Fig1H.clusters Barplots-sample_and_cluster-Anno.chi1.png", width = 12, height = 6, units = "in", res = 400) #只能是png
+png("Fig1H.clusters Barplots-sample_and_cluster-Anno.chi1.png", width = 12, height = 6, units = "in", res = 400)
 ggplot(data=dat_plot@meta.data, aes(x=sample_num, fill=cluster)) +
   geom_bar(stat="count", position="fill") +
   labs(x="", y="Proportion of cells") +
@@ -64,18 +55,18 @@ ggplot(data=dat_plot@meta.data, aes(x=sample_num, fill=cluster)) +
     axis.line = element_line(colour="black"),
     axis.text.x = element_text(angle=0, size=12, family="Times New Roman"),
     axis.text.y = element_text(angle=0, hjust=1, vjust=0.95, size=12, family="Times New Roman"),
-    axis.title.x = element_text(size=14, family="Times New Roman"),  # X轴标题字体
-    axis.title.y = element_text(size=14, family="Times New Roman"),  # Y轴标题字体
-    legend.text = element_text(size=14, family="Times New Roman"),  # 图例文字字体
-    legend.title = element_text(size=16, family="Times New Roman"), # 图例标题字体
-    plot.title = element_text(size=16, family="Times New Roman", face="bold")  # 图表标题字体
+    axis.title.x = element_text(size=14, family="Times New Roman"),  
+    axis.title.y = element_text(size=14, family="Times New Roman"),  
+    legend.text = element_text(size=14, family="Times New Roman"),  
+    legend.title = element_text(size=16, family="Times New Roman"), 
+    plot.title = element_text(size=16, family="Times New Roman", face="bold")  
   )
 
 dev.off()
 
 
 
-######------3.输出Source data---------#####
+######------3.export Source data---------#####
 dat <- MERGE
 dat$cluster <- factor(dat$Anno.chi1,levels = c("Excit_neuron", "Inhib_neuron", "Oligo", "Astro", "Micro/Macro", 
                                                "OPC", "Endo"))
